cf11ba56ff8fc13420fde52bf046e8407e126c78
JTipton25 <jtipton25@gmail.com>
2014-01-14 16:48:58 -0700

    updated MCMC code and write-up

--- spatial.script.R
+++ spatial.script.R
@@ -12,9 +12,10 @@ dinvgamma = function(x, shape = 1, rate = 1, scale = 1/rate, log = FALSE) {
     return(exp(logval))
 }
 
+##
+## Initialize priors and tuning paramteters
+##
 
-
-params = c(mu.0, Sigma.0, alpha.beta, beta.beta, alpha.eta, beta.eta, alpha.epsilon, beta.epsilon, alpha.phi, beta.phi, n.mcmc = 5000)
 mu.0 <- rep(0, dim(X)[2])
 sigma.squared.0 <- 100
 Sigma.0 <-
@@ -30,7 +31,7 @@ curve(dinvgamma(x, alpha.epsilon, beta.epsilon))
 alpha.phi <- 10
 beta.phi <- 10
 curve(dinvgamma(x, alpha.phi, beta.phi))
-n.mcmc <- 100
+n.mcmc <- 1000
 
 sigma.squared.beta.tune <- 0.025
 sigma.squared.eta.tune <- 0.025
@@ -39,19 +40,27 @@ phi.tune <- 0.25
 
 source('mcmc.spatial.R')
 
-start <-Sys.time()
+##
+## Fit spatial MCMC kriging model
+##
+
+start <- Sys.time()
 out <- mcmc.1d(Y.list, H.list, X, locs, n.mcmc, mu.0, Sigma.0, sigma.squared.beta, sigma.squared.eta, alpha.epsilon, beta.epsilon, alpha.beta, beta.beta, alpha.phi, beta.phi, mu.beta,  s.star, sigma.squared.eta.tune, sigma.squared.epsilon.tune, phi.tune)
 finish <- Sys.time() - start
 finish #100 iterations takes 8 minutes
 
+##
+## Plot output
+##
+
 layout(matrix(1:8, nrow = 2))
 plot(out$sigma.squared.beta.save, type = 'l')
-plot(out$sigma.squared.epsilon.save, type = 'l', main = paste("accept rate", out$epsilon.accept))
+plot(out$sigma.squared.epsilon.save, type = 'l', main = paste("accept rate", round(out$epsilon.accept, 2)))
 abline(h = s2.e)
-plot(out$sigma.squared.eta.save, type = 'l', main = paste("accept rate", out$eta.accept))
+plot(out$sigma.squared.eta.save, type = 'l', main = paste("accept rate", round(out$eta.accept, 2)))
 abline(h = s2.s)
 matplot(t(out$mu.beta.save), type = 'l')
-plot(out$phi.save, type = 'l', main = paste("accept rate", out$phi.accept))
+plot(out$phi.save, type = 'l', main = paste("accept rate", round(out$phi.accept, 2)))
 abline(h = phi)
 matplot(out$fort.raster, type = 'l')
 matplot(Z[, 1:100], type = 'l')
