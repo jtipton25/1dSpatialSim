8395c7ba1359b4bf11d59407a540b58b07940faf
jtipton25 <jtipton25@gmail.com>
2014-01-16 22:56:24 -0700

    Revert "added dinvgamma source and updated dinvgamma in code"
    This reverts commit 1b7963c9f04f08a64591c77ab4a162deff7c0f0f.

--- mcmc.spatial.R
+++ mcmc.spatial.R
@@ -30,8 +30,15 @@ mcmc.1d <- function(Y.list, H.list, X, locs, n.mcmc, mu.0, Sigma.0, alpha.epsilo
   ## Libraries and Subroutines
   ##
 
-	source('dinvgamma.R')
-  
+	dinvgamma = function(x, shape = 1, rate = 1, scale = 1/rate, log = FALSE) {
+		# return( rate^shape / gamma(shape) * exp( - rate / x) * x^( - shape - 1))
+		logval = shape * log(rate) - lgamma(shape) - rate / x - (shape + 1) * log(x)
+		if (log)
+			return(logval)
+		else
+			return(exp(logval))
+	}
+	
   make.sum.sigma.beta <- function(beta, mu.beta){
     temp <- vector(length = t)
     for(s in 1:t){
--- spatial.script.R
+++ spatial.script.R
@@ -5,7 +5,14 @@ set.seed(203)
 ## Libraries and Subroutines
 ##
 
-source('dinvgamma.R')
+dinvgamma = function(x, shape = 1, rate = 1, scale = 1/rate, log = FALSE) {
+  # return( rate^shape / gamma(shape) * exp( - rate / x) * x^( - shape - 1))
+  logval = shape * log(rate) - lgamma(shape) - rate / x - (shape + 1) * log(x)
+  if (log)
+    return(logval)
+  else
+    return(exp(logval))
+}
 
 ##
 ## Simulate Data
